c5a671d0415010a1dbc84184124c5aeecdc4600a
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-11-04 21:10:18 -0600

    child routing.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [],
+  },
+
+];
 
 @NgModule({
   imports: [ RouterModule.forChild(routes) ],
